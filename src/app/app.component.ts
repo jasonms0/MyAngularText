4485ff79d3b1dde5d8715471ab16da5d37f68b19
jasonms0 <84877177@qq.com>
2020-02-13 21:13:56 +0800

    ““first”

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+const todos = [{
+  id: 1,
+  title: "吃饭",
+  done: true
+},
+{
+  id: 2,
+  title: "睡觉",
+  done: false
+}]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'MyAngularText';
+  public todos: {
+    id: number;
+    title: string;
+    done: boolean;
+  }[] = todos;
+
+  public currentEditing: {
+    id: number,
+    title: string,
+    done: boolean
+  } = null;
+
+  public visibility: string = 'all'
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      /* 路由事件类型：
+        NavigationStart：导航开始
+        NavigationEnd：导航结束
+        NavigationCancel：取消导航
+        NavigationError：导航出错
+        RoutesRecoginzed：路由已认证
+      */
+      if (event instanceof NavigationEnd) {
+        const hash = window.location.hash.substr(1);
+        switch (hash) {
+          case '/':
+            this.visibility = 'all'
+            break;
+          case '/active':
+            this.visibility = 'active'
+            break;
+          case '/completed':
+            this.visibility = 'completed'
+            break;
+        }
+      }
+    })
+  }
+
+  get filterTodos() {
+    if (this.visibility === 'all') {
+      return this.todos
+    } else if (this.visibility === 'active') {
+      return this.todos.filter(t => !t.done)
+    } else if (this.visibility === 'completed') {
+      return this.todos.filter(t => t.done)
+    }
+  }
+
+  addTodo(e): void {
+    const titleText = e.target.value;
+
+    const last = this.todos[this.todos.length - 1];
+
+    this.todos.push({
+      id: last ? last.id + 1 : 1,
+      title: titleText,
+      done: false
+    });
+
+    e.target.value = ""
+  }
+
+  get toggleAll() {
+    return this.todos.every(item => item.done)
+  }
+
+  set toggleAll(val: boolean) {
+    this.todos.forEach(item => item.done = val)
+  }
+
+  removeTodo(i: number): void {
+    this.todos.splice(i, 1)
+  }
+
+  saveEdit(todo, e) {
+    todo.title = e.target.value;
+    this.currentEditing = null;
+  }
+
+  handleEditKeyUp(e) {
+    const { keyCode, target } = e;
+    if (keyCode == 27) {
+      target.value = this.currentEditing.title;
+      this.currentEditing = null
+    }
+  }
+
+  get remaningCount() {
+    return this.todos.filter(item => !item.done).length;
+  }
+
+  clearAllDone () {
+    this.todos = this.todos.filter(t => !t.done)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //引入FormsModule，可以在表单中使用ngModel(双向数据绑定)
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+}
